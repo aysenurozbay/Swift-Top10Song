a6915ec06c85590af2cdd52be59d71b9c4992312
aysenurozbay <aysenurozbay1004@gmail.com>
2022-12-29 21:21:53 +0300

    Initial Commit

---
+++ Favourite10YoutubeVideos/Favourite10YoutubeVideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Favourite10YoutubeVideosApp.swift
+//  Favourite10YoutubeVideos
+//
+//  Created by Ayşenur ÖZBAY on 29.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Favourite10YoutubeVideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
